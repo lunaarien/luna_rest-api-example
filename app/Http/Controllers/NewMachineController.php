e7ecde3fb364d23f377f961e8167e125d883dcfd
lunaarien <luna.denice.06232003@gmail.com>
2024-11-14 22:36:46 +0800

    Adding codes for 2relation tables sample

--- app/Http/Controllers/NewMachineController.php
+++ app/Http/Controllers/NewMachineController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\NewMachine;
+use App\Models\NewManufacturingAddress;
 use Illuminate\Http\Request;
 
 class NewMachineController extends Controller
 {
-    //
+    public function index (NewManufacturingAddress $ma) {
+        return $ma->newMachine;
+    }
 }
